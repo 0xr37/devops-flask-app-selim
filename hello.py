af84ab44e40a9c55b36681543566eaa9d609c298
Muhammad Selim <C24383413@mytudublin.ie>
2026-09-21 12:34:32 +0000

    Change greeting to Welcome

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def say_hello():
-    return '<p>Hello, World, I am a Flask app!</p><p><a href="/about">About this app</a></p><p><a href="/contact">Contact</a></p>'
+    return '<p>Welcome!</p><p><a href="/about">About this app</a></p><p><a href="/contact">Contact</a></p>'
 
 @app.route('/about')
 def about():
